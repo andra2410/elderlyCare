209ef0012afa59926f4ac190a4af32318eb4b199
Andra Urdea <urdeaandra22@stud.ase.ro>
2024-04-09 15:19:49 +0300

    Parent table for the users which will be ihnerited by caregivers and careseekers. The users table is abstract so it won't be created, but both caregivers and careseekers inherit the attributes from it (username, email, password...). +some changes to homepage.html

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///elderlycare.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/homepage')
@@ -16,6 +19,20 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+class Users(db.Model):
+    __abstract__ = True
+    # id = db.Column('caregiver_id', db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(60), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+
+
+class Caregivers(Users):
+    pass
 
 
 if __name__ == '__main__':
